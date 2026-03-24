38c076ba160541e46c8e2dda87257c928d05b8d2
dineshthambidurai-creator <dineshthambidurai@gmail.com>
2026-03-24 16:57:57 +0530

    Update turso_db.py

--- turso_db.py
+++ turso_db.py
@@ -1,15 +1,13 @@
-from libsql_client import create_client
+from libsql_client import create_client_sync
 import os
 
 _client = None
 
 def get_db():
     global _client
-
     if _client is None:
-        _client = create_client(
+        _client = create_client_sync(
             url=os.getenv("TURSO_DB_URL"),
             auth_token=os.getenv("TURSO_AUTH_TOKEN")
         )
-
     return _client
